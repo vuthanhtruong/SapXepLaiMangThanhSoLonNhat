84e2038082048fcb2bc45cb6bfdfba8af5242cb5
Vũ Thanh Trường <133890957+vuthanhtruong@users.noreply.github.com>
2024-08-12 07:45:53 +0700

    Update dsadsad.java

--- src/dsfsdf/dsadsad.java
+++ src/dsfsdf/dsadsad.java
@@ -4,12 +4,7 @@ public class dsadsad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int M[]=new int[] {33331,33};
-		int c=0;
-		for(int i=0;i<M.length;i++) {
-			System.out.print(M[i]+"; ");
-		}
-		System.out.println();
+		int M[]=new int[] {3, 39, 34, 38, 6, 399, 307, 308, 325, 312};
 		String N[]=new String[M.length];
 		for(int i=0;i<N.length;i++) {
 			N[i]=""+M[i];
@@ -27,52 +22,99 @@ public class dsadsad {
 				}
 			}
 		}
-		for(int i=0;i<M.length;i++) {
-			M[i]=Integer.parseInt(N[i]);
-		}
-		int count=0;
-		while(count<N.length-1) {
-			if(N[count].charAt(0)==N[count+1].charAt(0) ) {
-				if(N[count].length()>N[count+1].length()) {
-					int count1=0;
-					while(count1<N[count].length()-1) {
-						char chu =N[count].charAt(count1+1);
-						char chu1=N[count+1].charAt(0);
-						count1++;
-						int so=chu-'0';
-						int so1=chu1-'0';
-						if(so<so1) {
+
+		for(int i=0;i<M.length*M.length;i++) {
+			int count=0;
+			while(count<N.length-1) {
+				if(N[count].charAt(0)==N[count+1].charAt(0) ) {
+					if(N[count].length()>N[count+1].length()) {
+						String  chuoi1 = N[count].substring(0, N[count + 1].length());
+                        String  chuoi2 = N[count + 1].substring(0, N[count + 1].length());
+                        
+                        int chuso1=Integer.parseInt(chuoi1);
+                        int chuso2=Integer.parseInt(chuoi2);
+						int count1=0;
+						while(count1<N[count].length()-1) {
+							char chu1 =N[count].charAt(count1+1);
+							char chu2=N[count+1].charAt(0);
+
+							int so1=chu1-'0';
+							int so2=chu2-'0';
+							
+							if(so1<=so2) {
+								String teamp=N[count];
+								N[count]=N[count+1];
+								N[count+1]=teamp;
+								
+
+							}
+
+
+							if(chuso1>chuso2 ) {
+								String teamp=N[count];
+								N[count]=N[count+1];
+								N[count+1]=teamp;
+								break;
+
+							}
+							count1++;
+
+						}
+					}
+					else if (N[count].length()<N[count+1].length()) {
+						String  chuoi1 = N[count].substring(0, N[count].length());
+                        String  chuoi2 = N[count + 1].substring(0, N[count].length());
+                        
+                        int chuso1=Integer.parseInt(chuoi1);
+                        int chuso2=Integer.parseInt(chuoi2);
+						int count1=0;
+						while(count1<N[count+1].length()-1) {
+							char chu1 =N[count].charAt(0);
+							char chu2=N[count+1].charAt(count1+1);
+
+							int so1=chu1-'0';
+							int so2=chu2-'0';
+							
+							if(so1>=so2) {
+								String teamp=N[count];
+								N[count]=N[count+1];
+								N[count+1]=teamp;
+							}
+
+
+
+							if(chuso1<=chuso2) {
+								String teamp=N[count];
+								N[count]=N[count+1];
+								N[count+1]=teamp;
+								break;
+
+							}
+							count1++;
+
+						}
+					}
+					else if (N[count].length()==N[count+1].length()) {
+						int a=Integer.parseInt(N[count]);
+						int b=Integer.parseInt(N[count+1]);
+						if(a<b) {
 							String teamp=N[count];
 							N[count]=N[count+1];
 							N[count+1]=teamp;
-							break;
 						}
-					}
+						
+					} 
 				}
-				else {
-					int count1=0;
-					while(count1<N[count+1].length()-1) {
-						char chu =N[count+1].charAt(count1+1);
-						char chu1=N[count].charAt(0);
-						count1++;
-						int so=chu1-'0';
-						int so1=chu-'0';
-						if(so<so1) {
-							String teamp=N[count];
-							N[count]=N[count+1];
-							N[count+1]=teamp;
-							break;
-						}
-					}
-				}
+				count++;
 			}
-			count++;
 		}
+		
+		
 		System.out.println();
 		System.out.println("Từ các số đã cho ta ghép thành số lớn nhất là");
 		String SoLonNhat="";
 		for(int i=0;i<N.length;i++) {
-			SoLonNhat+=N[i];
+			SoLonNhat+=N[i]+" ";
 		}
 		System.out.println(SoLonNhat);
 	}
