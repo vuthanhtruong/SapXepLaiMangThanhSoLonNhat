c4e610fe89cc40aa730a367cd49c1130e4c1ffd4
Vũ Thanh Trường <133890957+vuthanhtruong@users.noreply.github.com>
2024-08-12 10:58:26 +0700

    Update dsadsad.java

--- src/dsfsdf/dsadsad.java
+++ src/dsfsdf/dsadsad.java
@@ -10,7 +10,7 @@ public class dsadsad {
 		//9, 91, 911, 912, 92
 		//15, 150, 151, 152
 		//1010, 101, 1001, 100
-		int M[]=new int[] {12, 121, 120, 1200, 12000, 3, 30, 12,34, 16,5, 9};
+		int M[]=new int[] {12, 121, 120, 1200, 12000, 3, 30, 12, 34, 16, 5, 9,209,219,2,21};
 		String N[]=new String[M.length];
 		for(int i=0;i<N.length;i++) {
 			N[i]=""+M[i];
@@ -28,14 +28,14 @@ public class dsadsad {
 				}
 			}
 		}
+		
 		for(int i=0;i<N.length;i++) {
 			for(int j=i+1;j<N.length;j++) {
 				char a1=N[i].charAt(0);
 				char a2=N[j].charAt(0);
 				int so1=a1-'0';
 				int so2=a2-'0';
-				if(so1==so2 && N[i].length()==N[j].length() && 
-					Integer.parseInt(N[i]) <= Integer.parseInt(N[j])) {
+				if(so1==so2 && N[i].length()>N[j].length()) {
 					String teamp=N[i];
 					N[i]=N[j];
 					N[j]=teamp;
@@ -48,7 +48,8 @@ public class dsadsad {
 				char a2=N[j].charAt(0);
 				int so1=a1-'0';
 				int so2=a2-'0';
-				if(so1==so2 && N[i].length()>N[j].length()) {
+				if(so1==so2 && N[i].length()==N[j].length() && 
+					Integer.parseInt(N[i]) <= Integer.parseInt(N[j])) {
 					String teamp=N[i];
 					N[i]=N[j];
 					N[j]=teamp;
