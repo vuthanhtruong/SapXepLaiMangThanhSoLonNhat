28eace4bdf9a56b94dfd6ea967dd6a6088dd978f
Vũ Thanh Trường <133890957+vuthanhtruong@users.noreply.github.com>
2024-08-12 08:57:22 +0700

    Update dsadsad.java

--- src/dsfsdf/dsadsad.java
+++ src/dsfsdf/dsadsad.java
@@ -4,26 +4,32 @@ public class dsadsad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int M[]=new int[] {3, 39, 34, 38, 6, 399, 307, 308, 325, 312};
+		//12, 121, 120, 1200, 12000, 3, 30, 34, 5, 9
+		//3, 30, 34, 5, 9
+		//12, 123, 1234, 12345
+		//9, 91, 911, 912, 92
+		//15, 150, 151, 152
+		//1010, 101, 1001, 100
+		int M[]=new int[] {3, 30, 34, 5,12,9,121, 120, 1200, 12000};
 		String N[]=new String[M.length];
 		for(int i=0;i<N.length;i++) {
 			N[i]=""+M[i];
 		}
-		for(int i=0;i<N.length;i++) {
-			for(int j=i+1;j<N.length;j++) {
-				char a1=N[i].charAt(0);
-				char a2=N[j].charAt(0);
-				int so1=a1-'0';
-				int so2=a2-'0';
-				if(so1<so2) {
-					String teamp=N[i];
-					N[i]=N[j];
-					N[j]=teamp;
+		
+		for(int i1=0;i1<M.length*M.length;i1++) {
+			for(int i=0;i<N.length;i++) {
+				for(int j=i+1;j<N.length;j++) {
+					char a1=N[i].charAt(0);
+					char a2=N[j].charAt(0);
+					int so1=a1-'0';
+					int so2=a2-'0';
+					if(so1<so2) {
+						String teamp=N[i];
+						N[i]=N[j];
+						N[j]=teamp;
+					}
 				}
 			}
-		}
-
-		for(int i=0;i<M.length*M.length;i++) {
 			int count=0;
 			while(count<N.length-1) {
 				if(N[count].charAt(0)==N[count+1].charAt(0) ) {
@@ -117,6 +123,8 @@ public class dsadsad {
 			SoLonNhat+=N[i]+" ";
 		}
 		System.out.println(SoLonNhat);
+	
+	
 	}
 
 }
